1b772d9c988b615feeef2a65bdbcc7dc0ccaf7f8
Uday <admin@udayteja.com>
2023-07-07 02:17:45 +0530

    Remove vercel json

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(bodyParser.json()); // used to parse the request and extract the informa
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+require("./routes")(app) // Initialize the route/s
+
 app.get("/", (req, res) => {
   const parseIp = (req) =>
     req.headers["x-forwarded-for"]?.split(",").shift() ||
@@ -27,7 +29,6 @@ app.get("/", (req, res) => {
   res.send(`Welcome ${parseIp(req)}, it's me ${os.hostname()}
   with ❤️ from San Francisco, USA (West) - sfo1`);
 });
-require("./routes")(app) // Initialize the route/s
 
 
 // Connect to the Database
