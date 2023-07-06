3904c11a1b67b90c0b493b09d907174c96712aae
Uday <admin@udayteja.com>
2023-07-07 02:33:45 +0530

    Remove fucking logs

--- index.js
+++ index.js
@@ -13,27 +13,25 @@ console.clear(); // clear the console to remove previous logging
 
 // Logs time for every request
 function requestTime(req, res, next) {
-    process.stdout.write(`Request-Date: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} IST, ${req.method} ${req.url} \n`);
-    next();
+  process.stdout.write(`Request-Date: ${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()} IST, ${req.method} ${req.url} \n`);
+  next();
 }
 
-// app.use(requestTime); // logs request time
+// TODO DO NOT USE CONSOLE LOGS FOR VERCEL PRODUCTION
+
+// app.use(requestTime); // logs request time 
 app.use(bodyParser.json()); // used to parse the request and extract the information
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+require("./routes")(app) // Initialize the route/s
 app.get("/", (req, res) => {
-  res.json({ message: "Hello From Express App" });
+  const parseIp = (req) =>
+    req.headers["x-forwarded-for"]?.split(",").shift() ||
+    req.socket?.remoteAddress;
+  res.send(`Welcome ${parseIp(req)}, it's me ${os.hostname()}
+  with ❤️ from San Francisco, USA (West) - sfo1`);
 });
-// require("./routes")(app) // Initialize the route/s
-// app.get("/", (req, res) => {
-//   const parseIp = (req) =>
-//     req.headers["x-forwarded-for"]?.split(",").shift() ||
-//     req.socket?.remoteAddress;
-//   res.send(`Welcome ${parseIp(req)}, it's me ${os.hostname()}
-//   with ❤️ from San Francisco, USA (West) - sfo1`);
-// });
-
 
 // Connect to the Database
 mongoose
@@ -41,22 +39,9 @@ mongoose
     useNewUrlParser: true, // To avoid Deprecation Warning
     useUnifiedTopology: true,
   })
-  .then(() => { 
+  .then(() => {
     app.listen(4000);
   })
   .catch((err) => {
     throw err;
   });
-
-
-// import express from "express";
-
-// const app = express();
-// const port = 9000;
-// app.use("/", (req, res) => {
-//   res.json({ message: "Hello From Express App" });
-// });
-
-// app.listen(9000, () => {
-//   console.log(`Starting Server on Port ${port}`);
-// });
